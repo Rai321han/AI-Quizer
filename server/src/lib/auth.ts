08bcce3ab82229a09a0ea8d05d21d9c850cf1c7f
Rai321han <uddinraihan797@gmail.com>
2025-10-22 14:03:54 +0600

    refactor: save-quiz initiliazation

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -1,13 +1,9 @@
 import { betterAuth } from "better-auth";
 import { Pool } from "pg";
+import pool from "../db";
 
 export const auth = betterAuth({
-  database: new Pool({
-    connectionString: process.env.DATABASE_URL,
-    max: 12,
-    min: 4,
-  }),
-
+  database: pool,
   trustedOrigins: ["http://localhost:3000"],
   emailAndPassword: {
     enabled: true,
