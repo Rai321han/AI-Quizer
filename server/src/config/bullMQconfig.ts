3fd9a9898699c4ab0ebebb695700b04491556851
Rai321han <uddinraihan797@gmail.com>
2025-10-31 03:56:55 +0600

    fix(queue): upstash redis config

--- server/src/config/bullMQconfig.ts
+++ server/src/config/bullMQconfig.ts
@@ -1,9 +1,11 @@
-import { Queue, Worker } from "bullmq";
+import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
 export const connection = new IORedis({
-  host: process.env.REDIST_HOST,
+  host: `${process.env.REDIS_ENDPOINT}`,
   port: Number(process.env.REDIS_PORT),
+  password: `${process.env.REDIS_PASSWORD}`,
   maxRetriesPerRequest: null,
+  tls: {},
 });
 export const quizQueue = new Queue("quizQueue", { connection });
