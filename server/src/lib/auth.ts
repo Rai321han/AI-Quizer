169b4f5f36c251eabc7bac94beefa66e8f58a927
Rai321han <uddinraihan797@gmail.com>
2025-10-31 19:50:34 +0600

    fix(auth-coookies): better-auth cross domain cookies

--- server/src/lib/auth.ts
+++ server/src/lib/auth.ts
@@ -4,8 +4,17 @@ import pool from "../db";
 
 export const auth = betterAuth({
   database: pool,
+
   trustedOrigins: ["http://localhost:3000", `${process.env.FRONTEND}`],
   emailAndPassword: {
     enabled: true,
+  },
+  advanced: {
+    crossSubDomainCookies: {
+      enabled: true,
+    },
+    defaultCookieAttributes: {
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "Lax",
+    },
   },
 });
